320d6c5faa868dcca58fa11c54722292c97ec7dc
ptzburn <milan.hokkanen@gmail.com>
2025-11-14 18:54:16 +0200

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,5 @@
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { FlatCompat } from '@eslint/eslintrc'
+import antfu from '@antfu/eslint-config'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+export default antfu({
+  react: true,
 })
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-  },
-]
-
-export default eslintConfig
