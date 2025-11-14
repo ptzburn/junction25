cb4e5c941587e108957135b38266623242edc5ea
ptzburn <milan.hokkanen@gmail.com>
2025-11-14 19:09:43 +0200

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,32 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  type: 'app',
+  typescript: true,
   react: true,
+  nextjs: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: 'double',
+  },
+  ignores: ['.pnpm-store/*'],
+}, {
+  rules: {
+    'ts/no-explicit-any': 'error',
+    'ts/no-redeclare': 'off',
+    'ts/consistent-type-definitions': ['error', 'type'],
+    'no-console': ['warn'],
+    'antfu/no-top-level-await': ['off'],
+    'node/prefer-global/process': ['off'],
+    'node/no-process-env': ['error'],
+    'perfectionist/sort-imports': ['error', {
+      tsconfigRootDir: '.',
+    }],
+    'unicorn/filename-case': ['error', {
+      case: 'kebabCase',
+      ignore: ['README.md'],
+    }],
+  },
 })
